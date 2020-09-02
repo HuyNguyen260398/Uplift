9b1f70d2ae698785821532efcb5c181fe49688a7
HuyNguyen260398 <huynguyen260398@gmail.com>
2020-09-03 06:28:10 +0700

    Implemented IRepository interface (added definitions)

--- Uplift.DataAccess/Data/Repository/IRepositoty/IRepository.cs
+++ Uplift.DataAccess/Data/Repository/IRepositoty/IRepository.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Uplift.DataAccess.Data.Repository.IRepositoty
 {
-    interface IRepository
+    // reposistory interface only contains definitions for others repositories implement
+
+    // using generic type T so that all different classes can implement
+
+    // generic type T is set as class because will be implemented by classes
+
+    public interface IRepository<T> where T : class
     {
+        T Get(int id);
+
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null
+            );
+
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = null
+            );
+
+        void Add(T entity);
+
+        void Remove(int id);
+
+        void Remove(T entity);
     }
 }
